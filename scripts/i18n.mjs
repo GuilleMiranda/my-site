a7d11061b981e9cdb082e68e40b799fafdc5680b
Guille <gdmn3000@gmail.com>
2024-02-22 23:00:36 -0300

    wip: translations

---
+++ scripts/i18n.mjs
@@ -0,0 +1,14 @@
+export const i18n = {
+    es: {
+        title: "Hola! Soy Guille &#128513;",
+        description: "Un desarrollador backend de Paraguay &#127477;&#127486;"
+    },
+    en: {
+        title: "This is me, Guille &#128513;",
+        description: "I am a backend developer from Paraguay &#127477;&#127486;"
+    },
+    pt: {
+        title: "Oi! Sou Guille &#128513;",
+        description: "Sou um desenvolvedor backend do Paraguay &#127477;&#127486;"
+    }
+};
---
+++ scripts/script.mjs
@@ -0,0 +1,17 @@
+import {i18n} from "./i18n.mjs";
+
+const supportedLanguages = ["es", "en", "pt"];
+const userPreferredLanguage = navigator.language.slice(0, 2);
+
+let language;
+if (supportedLanguages.includes(userPreferredLanguage)) {
+    language = userPreferredLanguage;
+} else {
+    language = "en";
+}
+
+let title = document.getElementById("title");
+let description = document.getElementById("description");
+
+title.innerHTML = i18n[language].title;
+description.innerHTML = i18n[language].description;
